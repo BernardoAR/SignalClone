391114dddf534a96cf3a9a3130eb9837266eb832
Bernardo Alves Roballo <bernardo_roballo@hotmail.com>
2021-07-10 14:38:41 -0300

    Made initial configurations

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDX1JL67-olB_vgQ7aRD2AN4TIbVu4XHGg',
+  authDomain: 'signal-clone-daca3.firebaseapp.com',
+  projectId: 'signal-clone-daca3',
+  storageBucket: 'signal-clone-daca3.appspot.com',
+  messagingSenderId: '597337316201',
+  appId: '1:597337316201:web:5535a53c8750c3f35fbbf8',
+};
